3bca8457efe26656ce3c999b44d0e038a45f8eff
ROOT233 <59696497+ROOT233@users.noreply.github.com>
2022-02-23 10:52:15 +0000

    Update Motor 2.0.cpp

--- Motor 2.0.cpp
+++ Motor 2.0.cpp
@@ -11,20 +11,20 @@ DigitalOut Bipolar_2(PB_4);
 PwmOut Motor_1(PB_5);
 PwmOut Motor_2(PB_3);
 //Global Variable
-float period = 0.4f;
+float period = 4;
 float volatile duty_cycle_1 = 0.25f;
 float volatile duty_cycle_2 = 0.25f;
 
 int main() {
  //Enable the motor driveboard
-    Enable_Driveboard = 1;
+    Enable_Driveboard = 0;
     //Use Bipolar mode
-    Bipolar_1 = 1;
-    Bipolar_2 = 1;
+    Bipolar_1 = 0;
+    Bipolar_2 = 0;
     //For A stationary motor requires exactly 50% duty cycle.
-    Motor_1.period(period); // 0.4 second period
+    Motor_1.period_ms(period); // 0.4 second period
     Motor_1.write(duty_cycle_1); // 25% duty cycle, relative to period
-    Motor_2.period(period); // 0.4 second period
+    Motor_2.period_ms(period); // 0.4 second period
     Motor_2.write(duty_cycle_2); // 25% duty cycle, relative to period
     
  while(1);
