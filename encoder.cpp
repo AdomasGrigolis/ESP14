bc55a211664d600d240c0c071d8215c5568e2002
AdomasGrigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-03-01 16:13:31 +0000

    Update encoder.cpp
    encoder pins fixed

--- encoder.cpp
+++ encoder.cpp
@@ -1,11 +1,16 @@
 //Use these object in the main code
-//QEI* wheel_left = new TickingEncoder(D8, D9, NC);
-//QEI* wheel_right = new TickingEncoder(D10, D11, NC);
+//QEI* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
+//QEI* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
 #include <QEI.h>
 
 #define WHEEL_CIRCUMFERENCE 276.46f//In milimetres
 #define ENCODER_MEASURE_PERIOD 1.0f//Encoder pulses are checked every 1 second
 #define NO_PULS_PER_REV 1024//Pulses per revolution (256*4)
+//Pins
+#define ENC_1_A_PIN PB_2
+#define ENC_1_B_PIN PB_15
+#define ENC_2_A_PIN PB_13
+#define ENC_2_B_PIN PB_14
 
 class TickingEncoder : public QEI {
     private:
@@ -18,7 +23,7 @@ class TickingEncoder : public QEI {
                 reset();//Resets the count
                 dx = dx + pulses/(44*WHEEL_CIRCUMFERENCE);//Old distance + new_speed*time=new_distance
         }
-        TickingEncoder(PinName channelA, PinName channelB, PinName index):QEI(channelA, channelB, index, NO_PULS_PER_REV)
+        TickingEncoder(PinName channelA, PinName channelB):QEI(channelA, channelB, NC, NO_PULS_PER_REV)
         {
             tick_enc.attach(callback(this, &TickingEncoder::enc_isr), ENCODER_MEASURE_PERIOD);
         }
