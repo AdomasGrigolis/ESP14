3fdf97f2951c2ac53f0d689b86191fbdd18a692f
AdomasGrigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-03-02 11:47:18 +0000

    Update encoder.cpp
    Fixed encoders outputing negative random value

--- encoder.cpp
+++ encoder.cpp
@@ -1,9 +1,10 @@
+//When importing the QEI library, in QEI.h change Encoding encoding = X2_ENCODING to X4_ENCODING in the constructor
 //Use these object in the main code
 //QEI* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
 //QEI* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
 #include <QEI.h>
 
-#define WHEEL_CIRCUMFERENCE 276.46f//In milimetres
+#define WHEEL_CIRCUMFERENCE 0.27646//In meters
 #define ENCODER_MEASURE_PERIOD 1.0f//Encoder pulses are checked every 1 second
 #define NO_PULS_PER_REV 1024//Pulses per revolution (256*4)
 //Pins
@@ -15,21 +16,22 @@
 class TickingEncoder : public QEI {
     private:
         Ticker tick_enc;
-        int pulses;
-        double dx;
+        volatile int pulses;
+        volatile double dx;
     public:
             void enc_isr(){//Called to fetch encoder data by tick_enc
                 pulses = getPulses();//Stores number of pulses
+                dx = pulses/(44.0*WHEEL_CIRCUMFERENCE);//Old distance + new_speed*time=new_distance
                 reset();//Resets the count
-                dx = dx + pulses/(44*WHEEL_CIRCUMFERENCE);//Old distance + new_speed*time=new_distance
         }
         TickingEncoder(PinName channelA, PinName channelB):QEI(channelA, channelB, NC, NO_PULS_PER_REV)
         {
+            dx = 0;
             tick_enc.attach(callback(this, &TickingEncoder::enc_isr), ENCODER_MEASURE_PERIOD);
         }
         //Selectors
-        int get_speed(){return pulses/(44*WHEEL_CIRCUMFERENCE)*ENCODER_MEASURE_PERIOD;}
-        int get_dx(){return dx;}
+        double get_speed(){return pulses/(44*WHEEL_CIRCUMFERENCE)*ENCODER_MEASURE_PERIOD;}
+        double get_dx(){return pulses;}
         //Modifiers
         void zero_dx(){dx=0;}
 };
