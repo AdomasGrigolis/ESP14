729d05220023aa2fc2b62986bde5fd11f64238b6
connor-pang <99603222+connor-pang@users.noreply.github.com>
2022-03-02 10:18:27 +0000

    Update TD1.cpp

--- TD1.cpp
+++ TD1.cpp
@@ -9,7 +9,7 @@ const float TURNING_CIRCLE = Pi*BUGGY_WIDTH*2.00;
 const float quarter_circle = TURNING_CIRCLE/4.00;
 //arc length for 180 degress
 const float half_circle = TURNING_CIRCLE/2.00;
-
+Car car();
 int main(void){
     QEI* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
     QEI* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
@@ -29,7 +29,7 @@ int main(void){
             }
             case driving:
             {   
-                Car.startMovingForward();
+                car.startMovingForward();
                 curr_pos=(wheel_left.get_dx()+wheel_right.get_dx())/2;
                 if(curr_pos> 500)
                     state = stop;
@@ -37,7 +37,7 @@ int main(void){
             }
             case stop:
             {   
-                Car.stop();
+                car.stop();
                 state = turning_initialisation;
                 break;    
             }
@@ -58,27 +58,27 @@ int main(void){
             }
             case turning_right:
             {   
-                Car.turn90Degrees(RIGHT);
+                car.turn90Degrees(RIGHT);
                 if(wheel_left.get_dx() > quarter_circle)
                     state = driving_initialisation;
                 break; 
             }
             case turning_left:
             {
-                Car.turn90Degrees(LEFT);
+                car.turn90Degrees(LEFT);
                 if(wheel_right.get_dx() > quarter_circle)
                     state = driving_initialisation;
                 break; 
             }
             case turning180:
             {
-                Car.turn180Degrees();
+                car.turn180Degrees();
                 if(wheel_left.get_dx() > half_circle) 
                 break;    
             }
             case finish:
             {
-                Car.stop();
+                car.stop();
                 break; 
             }
             default: 
