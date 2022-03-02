2534aa44df29514fe9be6a5aab3648db937462fe
AdomasGrigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-03-02 11:44:13 +0000

    Update PWMdrive.cpp
    +Fixed multiply defined issues with the class

--- PWMdrive.cpp
+++ PWMdrive.cpp
@@ -1,24 +1,16 @@
+//In main, use Car car;
+//Then to call functions, use car.XX(); or int integer car->get.XX;
 #include "mbed.h"
 #define FREQUENCY 18000.0f//Above audible range
 #define STATIONARY_DUTY_CYCLE 0.5f
 #define STARDARD_MOVEMENT_SPEED 0.5f
 //Pins
-#define ENABLE_PIN_OUR PA_13//Do not use ENABLE_PIN as it's already used in mbed
+#define ENABLE_PIN PA_13
 #define BIPOLAR1_PIN PH_1
 #define BIPOLAR2_PIN PH_0
 #define PWM_PIN1 PB_7
 #define PWM_PIN2 PA_15
-
-double PERIOD = 1.0f/FREQUENCY;//Period for PWM signal
-//Enable the Motor Driveboard
-DigitalOut Enable_Driveboard(ENABLE_PIN_OUR);
-
-//Use bipolar mode
-DigitalOut Bipolar_1(BIPOLAR1_PIN);
-DigitalOut Bipolar_2(BIPOLAR2_PIN);
-
-PwmOut Motor_1(PWM_PIN1);
-PwmOut Motor_2(PWM_PIN2);
+#define PERIOD 1.0f/FREQUENCY//Period for PWM signal
 
 enum Turn
 {
@@ -31,11 +23,17 @@ private:
         //PWM Out for motors
         float duty_cycle_1;//0.5 is stall
         float duty_cycle_2;
-
+        DigitalOut Enable_Driveboard;
+        DigitalOut Bipolar_1;
+        DigitalOut Bipolar_2;
+        PwmOut Motor_1;
+        PwmOut Motor_2;
 public:
-    Car()
-    {
-        Enable_Driveboard = 1;//Enable the motor driveboard
+    Car(PinName enable = ENABLE_PIN, PinName bipolar1 = BIPOLAR1_PIN, PinName bipolar2 = BIPOLAR2_PIN, PinName pwm1 = PWM_PIN1, PinName pwm2 = PWM_PIN2) :
+     Enable_Driveboard(enable), Bipolar_1(bipolar1), Bipolar_2(bipolar2), Motor_1(pwm1), Motor_2(pwm2)
+    {   
+        //Enable the motor driveboard
+        Enable_Driveboard = 1;
         //Use Bipolar mode
         Bipolar_1 = 1;
         Bipolar_2 = 1;
@@ -86,7 +84,7 @@ public:
     }
     void turn180Degrees()
     {
-        this->setMotorSpeeds(1.0f, -1.0f);
-        wait(0.1);
+       setMotorSpeeds(1.0f, 0.0f);
+            wait(0.1); 
     }
 };
