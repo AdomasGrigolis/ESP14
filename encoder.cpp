10fc0a9a58e24fde453cdfa3ee854a385366a83f
AdomasGrigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-03-03 15:13:04 +0000

    Update encoder.cpp
    Fixed dx and comments

--- encoder.cpp
+++ encoder.cpp
@@ -1,7 +1,7 @@
 //When importing the QEI library, in QEI.h change Encoding encoding = X2_ENCODING to X4_ENCODING in the constructor
 //Use these object in the main code
-//QEI* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
-//QEI* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
+//TickingEncoder* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
+//TickingEncoder* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
 #include <QEI.h>
 
 #define WHEEL_CIRCUMFERENCE 0.27646//In meters
@@ -21,8 +21,8 @@ class TickingEncoder : public QEI {
     public:
             void enc_isr(){//Called to fetch encoder data by tick_enc
                 pulses = getPulses();//Stores number of pulses
-                dx = pulses/(44.0*WHEEL_CIRCUMFERENCE);//Old distance + new_speed*time=new_distance
-                reset();//Resets the count
+                dx = pulses/(44.0*NO_PULS_PER_REV)*(NO_PULS_PER_REV/WHEEL_CIRCUMFERENCE);//Old distance + new_speed*time=new_distance
+//                reset();//Resets the count
         }
         TickingEncoder(PinName channelA, PinName channelB):QEI(channelA, channelB, NC, NO_PULS_PER_REV)
         {
@@ -31,7 +31,7 @@ class TickingEncoder : public QEI {
         }
         //Selectors
         double get_speed(){return pulses/(44*WHEEL_CIRCUMFERENCE)*ENCODER_MEASURE_PERIOD;}
-        double get_dx(){return pulses;}
+        double get_dx(){return dx;}
         //Modifiers
         void zero_dx(){dx=0;}
 };
