ecd2cee2cad9cb914349b27cc7f880c189849273
AdomasGrigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-03-07 14:11:33 +0000

    Update encoder.cpp
    Fixed distance function (formula)
    Added speed
    Moved isr to private

--- encoder.cpp
+++ encoder.cpp
@@ -4,8 +4,8 @@
 //TickingEncoder* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
 #include <QEI.h>
 
-#define WHEEL_CIRCUMFERENCE 0.27646//In meters
-#define ENCODER_MEASURE_PERIOD 1.0f//Encoder pulses are checked every 1 second
+#define WHEEL_CIRCUMFERENCE 251.33//In mm
+#define ENCODER_MEASURE_PERIOD 0.1f//Encoder pulses are checked every 1 second
 #define NO_PULS_PER_REV 1024//Pulses per revolution (256*4)
 //Pins
 #define ENC_1_A_PIN PB_2
@@ -18,19 +18,23 @@ class TickingEncoder : public QEI {
         Ticker tick_enc;
         volatile int pulses;
         volatile double dx;
-    public:
-            void enc_isr(){//Called to fetch encoder data by tick_enc
-                pulses = getPulses();//Stores number of pulses
-                dx += pulses/(44.0*NO_PULS_PER_REV)*(NO_PULS_PER_REV/WHEEL_CIRCUMFERENCE);//Old distance + new_speed*time=new_distance
-                reset();//Resets the count
+        volatile double speed;
+        void enc_isr()
+        {//Called to fetch encoder data by tick_enc
+            pulses = getPulses();//Stores number of pulses
+            dx += ((double)pulses/NO_PULS_PER_REV)*(WHEEL_CIRCUMFERENCE);//In mm
+            speed = dx/ENCODER_MEASURE_PERIOD;
+            reset();//Resets the count
         }
+    public:
         TickingEncoder(PinName channelA, PinName channelB):QEI(channelA, channelB, NC, NO_PULS_PER_REV)
         {
             dx = 0;
+            reset();
             tick_enc.attach(callback(this, &TickingEncoder::enc_isr), ENCODER_MEASURE_PERIOD);
         }
         //Selectors
-        double get_speed(){return pulses/(44*WHEEL_CIRCUMFERENCE)*ENCODER_MEASURE_PERIOD;}
+        double get_speed(){return speed;}
         double get_dx(){return dx;}
         //Modifiers
         void zero_dx(){dx=0;}
