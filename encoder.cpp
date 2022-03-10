0f2a1acd20115e2c4ccb800f58c052b3cf8d511e
AdomasGrigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-03-10 22:20:20 +0000

    Update encoder.cpp
    (Untested)
    Fixes to speed function

--- encoder.cpp
+++ encoder.cpp
@@ -2,10 +2,11 @@
 //Use these object in the main code
 //TickingEncoder* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
 //TickingEncoder* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
+//To get speed for e.g. use wheel_right->get_speed();
 #include <QEI.h>
 
-#define WHEEL_CIRCUMFERENCE 251.33//In mm
-#define ENCODER_MEASURE_PERIOD 0.1f//Encoder pulses are checked every 1 second
+#define WHEEL_CIRCUMFERENCE 251.33f//In mm
+#define ENCODER_MEASURE_PERIOD 0.001f//Encoder pulses are checked every 1 second
 #define NO_PULS_PER_REV 1024//Pulses per revolution (256*4)
 //Pins
 #define ENC_1_A_PIN PB_2
@@ -23,7 +24,7 @@ class TickingEncoder : public QEI {
         {//Called to fetch encoder data by tick_enc
             pulses = getPulses();//Stores number of pulses
             dx += ((double)pulses/NO_PULS_PER_REV)*(WHEEL_CIRCUMFERENCE);//In mm
-            speed = dx/ENCODER_MEASURE_PERIOD;
+            speed = (((double)pulses/NO_PULS_PER_REV)*(WHEEL_CIRCUMFERENCE))/ENCODER_MEASURE_PERIOD;
             reset();//Resets the count
         }
     public:
@@ -37,5 +38,5 @@ class TickingEncoder : public QEI {
         double get_speed(){return speed;}
         double get_dx(){return dx;}
         //Modifiers
-        void zero_dx(){dx=0;}
+        void zero_dx(){dx = 0;}
 };
