eea4e893057aba06550edb37ae656b7c7465f47c
Adomas Grigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-04-25 14:08:39 +0100

    Update buggymain.cpp

--- buggymain.cpp
+++ buggymain.cpp
@@ -7,10 +7,11 @@
 
 #define STOPPING_STIFFNESS 0.01f
 #define STOPPING_SPEED 0.1f
+#define DEFAULT_SPEED 6.0f//Rad/s
 
 //Desired angular speeds rad/s
-double global_ang_speed_right = 7.0;
-double global_ang_speed_left = 7.0;
+double global_ang_speed_right = DEFAULT_SPEED;
+double global_ang_speed_left = DEFAULT_SPEED;
 bool disable_speed_controls = false;
 
 // Max X value of LCD screen
@@ -19,6 +20,7 @@ bool disable_speed_controls = false;
 TickingEncoder* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
 TickingEncoder* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
 Potentiometer* leftPot = new SamplingPotentiometer(A0, 3.3, 100.0); 
+Potentiometer* rightPot = new SamplingPotentiometer(A1, 3.3, 100.0);
 Car car;//Motor control
 C12832 lcd(D11, D13, D12, D7, D10);
 //Line sensors
@@ -60,96 +62,102 @@ void stopGradual()
     decc_tick.attach(&deceleration, STOPPING_SPEED);
 }//Deceleration end
 
-float calculate_PID(float error_in, float kp){
-    float P_ = error_in *kp; //creates the p value by a given error(distance) and the controlling variable kp
-    float output = P_; //for future if I_ and D_ are implemented 
-    
-    if (output < 0){ //line is on the left hand side
-        global_ang_speed_left += output;
-        global_ang_speed_right -= output; //decreases duty cycle 1(right) and increases duty 2 (left)
-                                                  //this speeds up the right wheel and slows the left
-    }
-    else if (output > 0){
-        global_ang_speed_left -= output;
-        global_ang_speed_right += output; //increases duty1 (right) decreases duty2(left)
-                                                  //slows right wheel and speeds up the left
-    }
-    else{
-        global_ang_speed_left = 7.0;
-        global_ang_speed_right = 7.0;//remains constant forward
-        
-    }
-    return output; //returns the output for trouble shooting
-}
-
 int main(void)
 {
     lcd.set_auto_up(1);//This ensures no flickering on LCD
-    float Kp;
     int sensorsOn;
     float distance;
-    float volatile duty_delta;
+    float Kp;
+    float weight;
+    float step;
+    car.setMotorSpeeds(0.7f,0.7f);
     while(1){
+        distance = 0.0f;//mm
+        sensorsOn = 0;
         Kp = leftPot->getCurrentSampleNorm()/4.0f;//Up to 0.25
+        weight = rightPot->getCurrentSampleNorm()*5.0f;//Up to 5
+//Checking sensor states
+        if (sen1.sensorState() == true){
+            distance += 0.0f;//37
+            sensorsOn += 0;
+        }
+        if (sen2.sensorState() == true) {
+            distance += weight;//23
+            sensorsOn += 1;
+        }
+        if (sen3.sensorState() == true){
+            distance += 0.0f;//10
+            sensorsOn += 1;
+        }
+        if (sen4.sensorState() == true) {
+            distance -= 0.0f;//5
+            sensorsOn += 1;
+        }
+        if (sen5.sensorState() == true) {
+            distance -= weight;//19
+            sensorsOn += 1;
+        }
+        if (sen6.sensorState() == true){
+            distance -= 0.0f;//33
+            sensorsOn += 0;
+        }
+        if(sen3.sensorState() == true && sen4.sensorState() == true){
+            distance = 0.0f;
+            global_ang_speed_left = DEFAULT_SPEED;
+            global_ang_speed_right = DEFAULT_SPEED;
+        }
+        if (distance != 0.0f) step = 0.1;
+        else step = 0.02;
+        if (sensorsOn > 0) {
+            distance = distance/sensorsOn;
+            lcd.locate(50,0);
+            lcd.printf("%.0f",distance);
+            disable_speed_controls = false;
+            wheel_right->line();
+        }
+        /*
+        else {//No line
+            lcd.locate(50,0);
+            lcd.printf("NoL");
+            disable_speed_controls = true;
+            wheel_right->no_line();
+        }
+        */
+        float P_ = distance*Kp; //creates the p value by a given error(distance) and the controlling variable kp
+        float output = P_; //for future if I_ and D_ are implemented
+        if (output > 0) { //line is on the left hand side
+            global_ang_speed_left -= output;//speeds up right wheel and slows left
+            global_ang_speed_right += output;
+        } else if (output < 0) { //line is on the right hand side
+            global_ang_speed_left -= output;//slows right wheel and speeds up left
+            global_ang_speed_right += output;
+        } else {
+//            global_ang_speed_left = DEFAULT_SPEED;
+//            global_ang_speed_right = DEFAULT_SPEED;//remains constant forward
+        }
+//Safeguards so that speed doesn't go to negatives
+        if (global_ang_speed_left < 0.0f)global_ang_speed_left = 0.0f;
+        if (global_ang_speed_right < 0.0f)global_ang_speed_right = 0.0f;
+//Speed controller
+        if(disable_speed_controls == false) {
+            if(wheel_left->get_ang_speed() > global_ang_speed_left)car.changeDutyCycle(0.0f, step);
+            else if(wheel_left->get_ang_speed() < global_ang_speed_left)car.changeDutyCycle(0.0f, -1.0f*step);
+            if(wheel_right->get_ang_speed() > global_ang_speed_right)car.changeDutyCycle(step, 0.0f);
+            else if(wheel_right->get_ang_speed() < global_ang_speed_right)car.changeDutyCycle(-1.0f*step, 0.0f);
+        }
+        
+        //Data for troubleshooting
         lcd.locate(0,0);
         lcd.printf("Kp: %.3f",Kp);
+        lcd.locate(80,0);
+        lcd.printf("D:%.2f", output);
+        lcd.locate(50,10);
+        lcd.printf("Weight: %.1f",weight);
         lcd.locate(0,10);
         lcd.printf("%.1lf||%.1lf",wheel_left->get_ang_speed(), wheel_right->get_ang_speed());
         lcd.locate(0,20);
         lcd.printf("%.1lf||%.1lf",global_ang_speed_left, global_ang_speed_right);
-        distance = 0.0f;
-        sensorsOn = 0;
-        if (sen1.sensorState() == true){
-            distance += 45.0f;
-            sensorsOn += 1;
-        }
-//        else if (sen1.sensorState() == false){}
-        if (sen2.sensorState() == true){
-            distance += 25.0f;
-            sensorsOn += 1;
-        }
-//        else if (sen2.sensorState() == false){}
-        if (sen3.sensorState() == true){
-            distance += 10.0f;
-            sensorsOn += 1;
-        }
-//        else if (sen3.sensorState() == false){}
-        if (sen4.sensorState() == true){
-            distance -= 10.0f;
-            sensorsOn += 1;
-        }
-//        else if (sen4.sensorState() == false){}
-        if (sen5.sensorState() == true){
-            distance -= 25.0f;
-            sensorsOn += 1;
-        }
-//        else if (sen5.sensorState() == false){}
-        if (sen6.sensorState() == true){
-            distance -= 45.0f;
-            sensorsOn += 1;
-        }
-//        else if (sen6.sensorState() == false){}
-        if (sensorsOn > 0){
-            distance = distance/sensorsOn;
-            lcd.locate(50,0);
-            lcd.printf("%.0f",distance);
-            duty_delta = calculate_PID(distance, 0.0);
-            disable_speed_controls = false;
-            wheel_right->line();
-        }
-        else{
-            lcd.locate(50,0);
-            lcd.printf("No");
-            lcd.locate(50,10);
-            lcd.printf("line");
-            disable_speed_controls = true;
-            wheel_right->no_line(); 
-        }
-        if(disable_speed_controls == false){
-            if(wheel_left->get_ang_speed() > global_ang_speed_left)car.changeDutyCycle(0.0f, 0.05f);
-            else if(wheel_left->get_ang_speed() < global_ang_speed_left)car.changeDutyCycle(0.0f, -0.05f);
-            if(wheel_right->get_ang_speed() > global_ang_speed_right)car.changeDutyCycle(0.05f, 0.0f);
-            else if(wheel_right->get_ang_speed() < global_ang_speed_right)car.changeDutyCycle(-0.05f, 0.0f);
-        }
+        lcd.locate(50,20);
+        lcd.printf("DC:%.2f||%.2f",car.get_curr_dc(LEFT),car.get_curr_dc(RIGHT));
     }
 }
