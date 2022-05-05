3bb1238f9eec1d4809bb60a7412be0e38a180f9c
Adomas Grigolis <99285649+AdomasGrigolis@users.noreply.github.com>
2022-05-05 14:16:42 +0100

    Update buggymain.cpp
    added FSM

--- buggymain.cpp
+++ buggymain.cpp
@@ -5,18 +5,29 @@
 #include <pot.cpp>
 #include <sensor.cpp>
 
+#define SPEED_PERIOD 0.005f
+#define PID_PERIOD 0.01f
 #define DEFAULT_SPEED 8.0f//Rad/s
+
+//State machine
+typedef enum {idle, init, driving, bt_interrupt, bt_driving} ProgramState;
+ProgramState state;
 
 //Desired angular speeds rad/s
 double global_ang_speed_right = DEFAULT_SPEED;
 double global_ang_speed_left = DEFAULT_SPEED;
-bool disable_speed_controls = false;
-volatile float Kp;
+float Kp;
+float Kd;
+float Ki;
+
+//Tickers
+Ticker control_tick;
+Ticker speed_tick;
 
 TickingEncoder* wheel_left = new TickingEncoder(ENC_2_A_PIN, ENC_2_B_PIN);
 TickingEncoder* wheel_right = new TickingEncoder(ENC_1_A_PIN, ENC_1_B_PIN);
 Potentiometer* leftPot = new SamplingPotentiometer(A0, 3.3, 100.0); 
-Potentiometer* rightPot = new SamplingPotentiometer(A1, 3.3, 100.0);
+//Potentiometer* rightPot = new SamplingPotentiometer(A1, 3.3, 100.0);
 Car car;//Motor control
 C12832 lcd(D11, D13, D12, D7, D10);
 InterruptIn bluetooth_receive(PA_12);
@@ -28,29 +39,99 @@ SENSOR sen4(PC_2);//red
 SENSOR sen5(PC_3);//purple
 SENSOR sen6(PB_0);//orange (Power drop)
 
-Ticker Speed_tick;
+//Speed controller
 float step = 0.02f;
-void Speed_control(void){
-        if(disable_speed_controls == false) {      
-            if(wheel_left->get_ang_speed() > global_ang_speed_left)car.changeDutyCycle(0.0f, step);
-            else if(wheel_left->get_ang_speed() < global_ang_speed_left)car.changeDutyCycle(0.0f, -1.0f*step);
-            if(wheel_right->get_ang_speed() > global_ang_speed_right)car.changeDutyCycle(step, 0.0f);
-            else if(wheel_right->get_ang_speed() < global_ang_speed_right)car.changeDutyCycle(-1.0f*step, 0.0f);
-            //Safeguards so that speed doesn't go to negatives 
-            if (global_ang_speed_left < 0.0f)global_ang_speed_left = 0.0f;
-            if (global_ang_speed_right < 0.0f)global_ang_speed_right = 0.0f;
-        }
-}
+void Speed_control(void);//Below main
+
+//PID controller
 float pre_error = 0.0;
-Ticker control_tick;
+void control_PID(void);//Below main
+
+//Bluetooth
+class BLUETOOTH
+{
+private:
+    Serial hm10;//UART6 TX,RX
+    uint8_t i;
+    char hm10_receive_buffer[5];
+public:
+    BLUETOOTH(PinName USBTX, PinName USBRX): hm10(USBTX,USBRX)
+    {
+        hm10.baud(9600);
+    }
+    void control()
+    {
+        if(hm10.readable()) {
+            while(i < 5) {
+                hm10_receive_buffer[i++] = hm10.getc();
+            }
+            i = 0;
+            if(strcmp(hm10_receive_buffer, "ROUND") == 0) {
+                state = bt_interrupt;
+                memset(hm10_receive_buffer, 0, 5);
+            }
+            else if(strcmp(hm10_receive_buffer, "START") == 0){
+                if(state == idle)state = init;
+                memset(hm10_receive_buffer, 0, 5);
+            }
+        }
+    }
+};//End of Bluetooth
+BLUETOOTH BT(PA_11,PA_12);
+
+int main(void)
+{
+    lcd.set_auto_up(1);//This ensures no flickering on LCD
+    bluetooth_receive.rise(&BT, &BLUETOOTH::control);//Setup for BT
+    state = init;//Default starting state
+    //Proportional values
+    Kp = 0.7795f;
+    Kd = 0.0088f;
+    Ki = 0.0f;
+    while(true){
+//State switch                            
+    switch (state) {
+        case(idle) :
+                Kp = leftPot->getCurrentSampleNorm()*2.0f;//Up to 2
+                lcd.locate(0,0);
+                lcd.printf("%.1f /100",Kp*100);
+                break;
+        case(init) :
+                car.setDirectionForward();
+                speed_tick.attach(&Speed_control, SPEED_PERIOD);
+                control_tick.attach(&control_PID, PID_PERIOD);
+                state = driving;
+                break;  
+        case(driving) :
+                if(wheel_right->get_flag() == true){//Controlled stop
+                    global_ang_speed_right = 0.0;
+                    global_ang_speed_left = 0.0;
+                }
+                break;
+        case(bt_interrupt) :
+                control_tick.detach();
+                global_ang_speed_right = 0.0;
+                global_ang_speed_left = 0.0;
+                speed_tick.detach();
+                car.turnaround(RIGHT);//Might need to change side
+                wheel_right->quarter_circle();
+                car.setMotorSpeeds(0.8f,0.8f);
+                state = bt_driving;
+                break;  
+        case(bt_driving) :
+                if(wheel_right->get_flag() == true)state=init;
+                break; 
+        default :
+                state = idle;
+                break;
+        }
+    }
+}
+
 void control_PID(void)
 {
         int sensorsOn = 0;
-        
-        float distance = 0.0f;
-        Kp = leftPot->getCurrentSampleNorm()*2.0f;//Up to 0.5
-        float Kd = 0.0088f;
-        
+        float distance = 0.0f;     
 //Checking sensor states
         if (sen1.sensorState() == true){
             distance += 3.0f;//37,3
@@ -83,12 +164,11 @@ void control_PID(void)
         }
         
         if (sensorsOn > 0) {
+            wheel_right->line();//Alerts enc that we have a line
             distance = distance/sensorsOn;
-            float D__ = (distance - pre_error)/0.01f;
+            float D__ = (distance - pre_error)/PID_PERIOD;
             float D_ = D__ * Kd;
             pre_error = distance;
-            disable_speed_controls = false;
-            wheel_right->line();
             float P_ = distance*Kp; //creates the p value by a given error(distance) and the controlling variable kp
             float output = P_ + D_; //for future if I_ and D_ are implemented
             if (output > 0) { //line is on the left hand side
@@ -97,74 +177,17 @@ void control_PID(void)
             } else if (output < 0) { //line is on the right hand side
                 global_ang_speed_left = DEFAULT_SPEED-output;//slows right wheel and speeds up left
                 global_ang_speed_right = DEFAULT_SPEED+output;
-            } else {
-//              global_ang_speed_left = DEFAULT_SPEED;
-//              global_ang_speed_right = DEFAULT_SPEED;//remains constant forward
             }
         }
-        else {//No line
-            wheel_right->no_line();
-        }
+        else{wheel_right->no_line();}//No line
 }
 
-//Bluetooth
-class BLUETOOTH
-{
-private:
-    Serial hm10; //UART6 TX,RX
-    uint8_t i;
-    char hm10_receive_buffer[5];
-public:
-    BLUETOOTH(PinName USBTX, PinName USBRX): hm10(USBTX,USBRX)
-    {
-        hm10.baud(9600);
-    }
-    void control()
-    {
-        if(hm10.readable()) {
-            while(i < 5) {
-                hm10_receive_buffer[i++] = hm10.getc();
-            }
-            i = 0;
-            if(strcmp(hm10_receive_buffer, "ROUND") == 0) {
-                disable_speed_controls = true;
-                control_tick.detach();
-                global_ang_speed_right = 0.0;
-                global_ang_speed_left = 0.0;
-                car.setMotorSpeeds(0.8f,0.8f);
-                car.turnaround(LEFT);
-                memset(hm10_receive_buffer, 0, 5);
-            }
-            else if(strcmp(hm10_receive_buffer, "PAUSE") == 0){
-                control_tick.detach();
-                global_ang_speed_right = 0.0;
-                global_ang_speed_left = 0.0;
-                memset(hm10_receive_buffer, 0, 5);
-            }
-            else if(strcmp(hm10_receive_buffer, "RESUM") == 0){
-                car.setDirectionForward();
-                disable_speed_controls = false;
-                control_tick.attach(&control_PID, 0.01);
-                memset(hm10_receive_buffer, 0, 5);
-                }
-        }
-    }
-};//End of Bluetooth
-BLUETOOTH BT(PA_11,PA_12);
-
-
-int main(void)
-{
-    lcd.set_auto_up(1);//This ensures no flickering on LCD
-    Speed_tick.attach(&Speed_control, 0.005);
-    control_tick.attach(&control_PID, 0.01);
-    bluetooth_receive.rise(&BT, &BLUETOOTH::control);
-    while(true){
-        lcd.locate(0,0);
-        lcd.printf("%.2f /100",Kp*100);
-        if(wheel_right->get_flag() == true){//Controlled stop
-            global_ang_speed_right = 0.0;
-            global_ang_speed_left = 0.0;
-        }
-    }
+void Speed_control(void){
+        if(wheel_left->get_ang_speed() > global_ang_speed_left)car.changeDutyCycle(0.0f, step);
+        else if(wheel_left->get_ang_speed() < global_ang_speed_left)car.changeDutyCycle(0.0f, -1.0f*step);
+        if(wheel_right->get_ang_speed() > global_ang_speed_right)car.changeDutyCycle(step, 0.0f);
+        else if(wheel_right->get_ang_speed() < global_ang_speed_right)car.changeDutyCycle(-1.0f*step, 0.0f);
+        //Safeguards so that speed doesn't go to negatives 
+        if (global_ang_speed_left < 0.0f)global_ang_speed_left = 0.0f;
+        if (global_ang_speed_right < 0.0f)global_ang_speed_right = 0.0f;
 }
